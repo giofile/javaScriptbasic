ce9496fa62c4a53846bb990f629a84f8d81dd716
giofile <77681874+giofile@users.noreply.github.com>
2021-07-20 14:35:48 +0200

    Add files via upload
    oefenen met Javascript
    
    practising Javascript

---
+++ script.js
@@ -0,0 +1,36 @@
+const name= "Giovanni";
+console.log(name);
+console.log("Hello Winc Academy");
+
+const numberOne = 4;
+const numberTwo = 4;
+const numberThree = "4";
+const numberFour = "4";
+
+
+const optellen =  numberOne + numberTwo;
+console.log(optellen);
+
+const optellenTwee = numberThree + numberFour
+console.log(optellenTwee);
+// string wordt samengevoegd  in plaats van opgeteld
+
+const subtraction = numberOne - numberTwo;
+console.log(subtraction);
+
+const multiplication =numberOne * numberTwo;
+console.log(multiplication);
+
+
+const division = numberOne / numberTwo;
+console.log(division);
+
+const modulus = numberOne % numberTwo;
+console.log(modulus);
+
+let age = 47;
+console.log(typeof(age));
+
+let ageTwo =" Ik ben 47";
+console.log(typeof(ageTwo));
+
